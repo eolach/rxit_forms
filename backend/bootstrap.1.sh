6b89d4ae8554c0c71018f32332052e8ed51341a2
Neil McEvoy <neil@eolas.ca>
2018-10-19 10:05:19 -0400

    -a.gitignore fix

--- backend/bootstrap.1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export FLASK_APP=rxit #/main.py
-source $(pipenv --venv)/bin/activate
-flask shell
--- backend/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export FLASK_APP=rxit #/main.py
-export FLASK_ENV=development
-source $(pipenv --venv)/bin/activate
-flask run -h 0.0.0.0
