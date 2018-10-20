eb5f159f731137b080088b0ffba5cb652de577a5
Neil McEvoy <neil@eolas.ca>
2018-10-20 02:49:47 -0400

    Add target for stage env.

--- rxit-forms/src/environments/environment.prod.ts
+++ rxit-forms/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://rxitstudy.com:5000'
+  API_URL: 'http://rxitstudy.com'
 };
--- rxit-forms/src/environments/environment.stage.ts
+++ rxit-forms/src/environments/environment.stage.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:5000'
+  API_URL: 'http://symulationstage.com'
 
 };
 
